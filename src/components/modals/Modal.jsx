da267e0adb3558dd8ded9ad508c2c5f9b1787267
risyad <risyadzyn@gmail.com>
2021-12-24 00:53:05 +0700

    show detail

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,19 +1,34 @@
+import api from "../../services/api";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+function Modal({ setOpenModal, pokemon }) {
+  const [pokemonDetails, setPokemonDetails] = useState({});
 
-function Modal({ setOpenModal }) {
+  useEffect(() => {
+    axios.get(pokemon.url).then((res) => {
+      setPokemonDetails(res.data);
+    });
+  }, [pokemon]);
+
   return (
-    <div className="modalBackground"
+    <div
+      className="modalBackground"
       onClick={() => {
         setOpenModal(false);
-      }}>
-      <div className="modalContainer"
-        onClick={e => {
+      }}
+    >
+      <div
+        className="modalContainer"
+        onClick={(e) => {
           e.stopPropagation();
-        }}>
-      
+        }}
+      >
+        <h1>modal</h1>
+        <h1>{pokemonDetails.name}</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
--- src/components/pokemon-list/PokemonList.jsx
+++ src/components/pokemon-list/PokemonList.jsx
@@ -1,29 +1,54 @@
-import api from '../../services/api'
-import {useEffect, useState} from 'react'
+import api from "../../services/api";
+import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import Modal from "../modals/Modal";
 
 function PokemonList() {
-  const [items, setItems] = useState(0)
-  const [pokemons, setPokemons] = useState([])
-  
+  const [items, setItems] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+  const [activePokemon, setActivePokemon] = useState({});
+  const [modalOpen, setModalOpen] = useState(false);
+
   useEffect(() => {
-    api.get(`/pokemon?offset=${items}&limit=100`)
-    .then(res => {
-      setPokemons(res.data.results)
-    })
-  },[])
+    api.get(`/pokemon?offset=${items}&limit=100`).then((res) => {
+      setPokemons([...pokemons, ...res.data.results]);
+    });
+  }, [items]);
+
+  const dataModal = (pokemon) => {
+    setModalOpen(true);
+    setActivePokemon(pokemon);
+  };
 
   return (
-    <div className='poke-container'>
-      {pokemons.sort((a, b) => a.name.localeCompare(b.name)).map(pokemon => {
-        return (
-          <div key={pokemon.name} className='poke-list'>
-            <div className='poke-name'>{pokemon.name}</div>
-          </div>
-        )
-      })}  
+    <div>
+      <InfiniteScroll
+        dataLength={pokemons.length}
+        next={() => setItems(items + 100)}
+        hasMore={true}
+        className="poke-container"
+      >
+        {pokemons
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((pokemon) => {
+            return (
+              <div
+                key={pokemon.name}
+                className="poke-list"
+                onClick={() => {
+                  dataModal(pokemon);
+                }}
+              >
+                <div className="poke-name">{pokemon.name}</div>
+              </div>
+            );
+          })}
+      </InfiniteScroll>
+      {modalOpen && (
+        <Modal setOpenModal={setModalOpen} pokemon={activePokemon} />
+      )}
     </div>
-  )
+  );
 }
 
-export default PokemonList
+export default PokemonList;
