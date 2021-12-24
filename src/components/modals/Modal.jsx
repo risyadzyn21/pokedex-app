af20f4820ea906a74160e89805d078beda231bd8
risyad <risyadzyn@gmail.com>
2021-12-24 14:13:14 +0700

    95% done

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -24,8 +24,75 @@ function Modal({ setOpenModal, pokemon }) {
           e.stopPropagation();
         }}
       >
-        <h1>modal</h1>
-        <h1>{pokemonDetails.name}</h1>
+        <div className="pokemon-header">
+          <div className="pokemon-img">
+            <img
+              src={pokemonDetails?.sprites?.front_default}
+              alt={pokemonDetails?.name}
+            />
+          </div>
+          <div className="pokemon-info">
+            <div>#{pokemonDetails.id}</div>
+            <h2>{pokemonDetails.name}</h2>
+            <div className="pokemon-info-adv">
+              <div>
+                Base Exp <span>{pokemonDetails.base_experience}</span>
+              </div>
+              <div>
+                Weight <span>{pokemonDetails.weight}</span>
+              </div>
+              <div>
+                Height <span>{pokemonDetails.height}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pokemon-details">
+          <div className="pokemon-details-left">
+            <div className="pokemon-details-stats">
+              <div className="pokemon-details-title">Stats</div>
+              {pokemonDetails?.stats?.map((stat) => {
+                return (
+                  <div key={stat.stat.name}>
+                    <div>{stat.stat.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="pokemon-details-types">
+              <div className="pokemon-details-title">Types</div>
+              {pokemonDetails?.types?.map((type) => {
+                return (
+                  <div key={type.type.name}>
+                    <div>{type.type.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="pokemon-details-abilities">
+              <div className="pokemon-details-title">Abilities</div>
+              {pokemonDetails?.abilities?.map((ability) => {
+                return (
+                  <div key={ability.ability.name}>
+                    <div>{ability.ability.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="pokemon-details-right">
+            <div className="pokemon-details-moves">
+              <div className="pokemon-details-title">Moves</div>
+              {pokemonDetails?.moves?.map((move) => {
+                return (
+                  <div key={move.move.name}>
+                    <div>{move.move.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-import PokemonList from "../components/pokemon-list/PokemonList"
-
+import PokemonList from "../components/pokemon-list/PokemonList";
 
 function Home() {
   return (
     <div className="poke-home">
-      <h1 className="home-title">Pokedex App</h1>
-      <PokemonList/>
+      <h1 className="home-title">Pokédex App</h1>
+      <PokemonList />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
